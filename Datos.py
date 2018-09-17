956ffa9a60d20133cdd91d312d356734f57cf318
javier encinas <javier.encinasc@estudiante.uam.es>
2018-09-17 21:50:00 +0200

    primer commit

---
+++ Datos.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class Datos(object):
+  
+  TiposDeAtributos=('Continuo','Nominal')
+ 
+  # TODO: procesar el fichero para asignar correctamente las variables tipoAtributos, nombreAtributos, nominalAtributos, datos y diccionarios
+  def __init__(self, nombreFichero):
+    
+  # TODO: implementar en la práctica 1
+  def extraeDatos(idx):
+    pass
+
+
+  
---
+++ MainDatos.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 01 20:23:59 2017
+
+@author: profesores faa
+"""
+
+from Datos import Datos
+
+#dataset=Datos('f:/temp/german.data')
+dataset=Datos('f:/temp/tic-tac-toe.data')
