2371356de4cea4b7b1be9640fbdf5256889e4481
Javier Encinas Cortés <javiec93encinas@gmail.com>
2018-09-21 20:22:43 +0200

    al menos hemos hecho algo

--- Datos.py
+++ Datos.py
@@ -6,8 +6,39 @@ class Datos(object):
  
   # TODO: procesar el fichero para asignar correctamente las variables tipoAtributos, nombreAtributos, nominalAtributos, datos y diccionarios
   def __init__(self, nombreFichero):
-    
-  # TODO: implementar en la pr·ctica 1
+  	with open(nombreFichero, "r") as f:
+			f.replace("/n","");
+			numFilas=atoi(f.readline());
+			atributos=f.readline();
+			tipos=f.readline().split(",");
+			posiciones=[];
+			diccionario=[];
+
+		i=0;
+		for x in tipos:
+			ele = [];
+			diccionario.append(ele);
+
+			if x == "Nominal":
+				posiciones.append(i);
+				i++;
+
+		for x in f:
+			fila = x.split(",");
+			i=0;
+			for y in fila:
+				if i in posiciones and y not in diccionario[i]:
+					diccionario[i].append(y);
+				i++;
+
+		for element in diccionario:
+			element.tolist();
+			element.sort();
+			elemant.toarray();
+		
+
+    		
+  # TODO: implementar en la pr√°ctica 1
   def extraeDatos(idx):
     pass
 
