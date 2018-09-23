ff058d08ee3cc739a445dbd282b9913ff464f76c
pelofeo <urdiales.guillermo@gmail.com>
2018-09-23 04:51:11 -0700

    Cambiar los atois

--- Datos.py
+++ Datos.py
@@ -2,45 +2,46 @@ import numpy as np
 
 class Datos(object):
   
-  TiposDeAtributos=('Continuo','Nominal')
+	TiposDeAtributos=('Continuo','Nominal')
  
   # TODO: procesar el fichero para asignar correctamente las variables tipoAtributos, nombreAtributos, nominalAtributos, datos y diccionarios
-  def __init__(self, nombreFichero):
-  	with open(nombreFichero, "r") as f:
-			f.replace("/n","");
-			numFilas=atoi(f.readline());
+	def __init__(self, nombreFichero):
+		with open(nombreFichero, "r") as f:
+			file =f.read();
+			file.replace("/n","");
+			numFilas=f.readline().atoi();
 			atributos=f.readline();
 			tipos=f.readline().split(",");
 			posiciones=[];
 			diccionario=[];
 
-		i=0;
-		for x in tipos:
-			ele = [];
-			diccionario.append(ele);
-
-			if x == "Nominal":
-				posiciones.append(i);
-				i++;
-
-		for x in f:
-			fila = x.split(",");
 			i=0;
-			for y in fila:
-				if i in posiciones and y not in diccionario[i]:
-					diccionario[i].append(y);
-				i++;
+			for x in tipos:
+				ele = [];
+				diccionario.append(ele);
 
-		for element in diccionario:
-			element.tolist();
-			element.sort();
-			elemant.toarray();
-		
+				if x == "Nominal":
+					posiciones.append(i);
+					i=+1;
+
+			for x in f:
+				fila = x.split(",");
+				i=0;
+				for y in fila:
+					if i in posiciones and y not in diccionario[i]:
+						diccionario[i].append(y);
+					i=+1;
+
+			for element in diccionario:
+				element.tolist();
+				element.sort();
+				element.toarray();
+				print(element);
 
     		
   # TODO: implementar en la práctica 1
-  def extraeDatos(idx):
-    pass
+	def extraeDatos(idx):
+		pass
 
 
   
